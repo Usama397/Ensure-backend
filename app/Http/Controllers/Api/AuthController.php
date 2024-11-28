ae294dc156e9b1daf2cb9df71473cc51f36e6506
Faisal khan <faisalkhan@pitb.gov.pk>
2024-11-28 23:09:20 +0500

    dd change

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -246,7 +246,7 @@ class AuthController extends Controller
 
         $validator = Validator::make($request->all(), [
             'name' => 'required|string|max:255',
-            'email' => 'required|email|unique:users,email,' . $user->id,
+            'email' => 'required',
             'password' => 'required|min:8|confirmed',
             'phone_no' => 'required|string',
         ]);
